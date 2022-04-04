c814b0588cc1853ed153f77da1faaee8919d2982
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-04-04 22:11:54 +0200

    add function for different level

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,31 +6,51 @@ con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro. */
 
-/* const btn = document.getElementById ("play");
+const btn = document.getElementById ("play");
 
 btn.addEventListener ("click", function GenerateGrid(selector){
 
-    console.log("click")
+    /* console.log("click") */
+
+    let difficult = document.querySelector(".selection").value ;
+
+    if (difficult == "Easy"){
+        console.log(difficult)
+        
+        generateGrid(".cells", "div", "cell", 100);
+        selectElements (".cell", "active")
+        
+
+    }
+
+    else if (difficult == "Medium" ){
+        generateGrid(".cells", "div", "cell", 81);
+        selectElements (".cell", "active")
+        
+
+    }
+
+    else if (difficult == "Hard") {
+        generateGrid(".cells", "div", "cell", 49);
+        selectElements (".cell", "active")
 
     
-
-   
-    
-
-  
+    }
 
 
-}) */
+}) 
 
 
 
 function generateGrid (selector, tag_name, class_name, limit) {
 
     const cellsElement = document.querySelector(selector)
+    cellsElement.innerHTML = " " ;
     
     for(let i = 1; i <= limit; i++) {
 
-        const cell = document.createElement(tag_name);
+        let cell = document.createElement(tag_name);
+       
         cell.classList.add(class_name);
         cellsElement.append(cell);
 
@@ -40,8 +60,6 @@ function generateGrid (selector, tag_name, class_name, limit) {
 
 
 
-
-generateGrid(".cells", "div", "cell", 100);
 
 
 function selectElements (selector, active_class){
@@ -64,7 +82,7 @@ function selectElements (selector, active_class){
         cellItem.addEventListener("click", function(){
 
             console.log(this, i);
-            this.classList.add(active_class);
+            this.classList.toggle(active_class);
         })
     }
 
@@ -74,7 +92,6 @@ function selectElements (selector, active_class){
 
 
 
-selectElements (".cell", "active")
 
 
 
