51989482d98bea53b2a6447e4907e03fbd97caa1
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-04-04 20:00:23 +0200

    add functions for generate grid and select cells

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,16 +6,67 @@ con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro. */
 
-const btn = document.getElementById ("play");
+/* const btn = document.getElementById ("play");
 
-btn.addEventListener ("click", function(){
-    /* console.log("click") */
+btn.addEventListener ("click", function GenerateGrid(selector){
+
+    console.log("click")
 
     
 
+   
+    
+
+  
+
+
+}) */
+
+
+
+function generateGrid (selector, tag_name, class_name, limit) {
+
+    const cellsElement = document.querySelector(selector)
+    
+    for(let i = 1; i <= limit; i++) {
+
+        const cell = document.createElement(tag_name);
+        cell.classList.add(class_name);
+        cellsElement.append(cell);
+
+    }
+
+}
 
 
 
 
+generateGrid(".cells", "div", "cell", 100);
 
-})
+
+function selectElements (selector, active_class){
+    //creo una variabile in cui c'è una lista con tutte le mie celle
+
+    const cells = document.querySelectorAll(selector);
+
+    //creo un ciclo for il cui limite è la lunghezza della lista di celle, che mi permetterà di selezionare le singole celle della lista
+
+    for (let i = 0; i < cells.length; i++) {
+        const cellItem = cells[i];
+        /* console.log(cellItem); */
+
+
+        //creo una funzione che mi permette di aggiungere al singolo oggetto un evento al click
+        cellItem.addEventListener("click", function(){
+
+        console.log(this, i);
+        this.classList.add(active_class);
+    })
+
+}
+
+}
+
+
+selectElements (".cell", "active")
+
